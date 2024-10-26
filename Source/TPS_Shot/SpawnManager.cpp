c6e29007027c9ba39574895bea9f9bed0a899379
DAIKI\kis11 <kis1181100133@gmail.com>
2024-10-26 18:36:59 +0900

    スポーンを扱うクラスを作成

---
+++ Source/TPS_Shot/SpawnManager.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnManager.h"
+
+USpawnManager::USpawnManager()
+{
+}
+
+void USpawnManager::SetUp(const FActorSpawnParameters& spawnParameter, const FVector location, const FRotator rotation, const FVector scale)
+{
+	_spawnData.SpawnParameter = spawnParameter;
+	_spawnData.Location = location;
+	_spawnData.Rotation = rotation;
+	_spawnData.Scale = scale;
+}
+
+AActor* USpawnManager::SpawnActor(TSubclassOf<AActor> actorClass)
+{
+	if (!actorClass)
+	{
+		return nullptr;
+	}
+
+	UWorld* world = GetWorld();
+	if (world)
+	{
+		FTransform spawnTransform(_spawnData.Rotation, _spawnData.Location, _spawnData.Scale);
+
+		AActor* spawnActor = world->SpawnActor<AActor>(
+			actorClass,
+			spawnTransform,
+			_spawnData.SpawnParameter);
+
+		if (spawnActor)
+		{
+			return spawnActor;
+		}	
+	}
+
+	return nullptr;
+}
---
+++ Source/TPS_Shot/SpawnManager.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SpawnManager.generated.h"
+
+USTRUCT()
+struct FSpawnData
+{
+	GENERATED_BODY()
+
+	FActorSpawnParameters SpawnParameter;
+	FVector Location;
+	FRotator Rotation;
+	FVector Scale;
+};
+
+UCLASS()
+class TPS_SHOT_API USpawnManager : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	USpawnManager();
+
+	void SetUp(const FActorSpawnParameters& spawnParameter,
+		const FVector location = FVector::ZeroVector,
+		const FRotator rotation = FRotator::ZeroRotator,
+		const FVector scale = FVector::ZeroVector);
+
+	AActor* SpawnActor(TSubclassOf<AActor> actorClass);
+
+private:
+	FSpawnData _spawnData;
+};
